684bbb2b120ebba3f57d9016d3bffd53a7217ab3
Gerrit Uitslag <klapinklapin@gmail.com>
2013-09-16 13:40:04 +0200

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
+ */
+$lang['jiraserver']            = 'Jira issues browser URL <br />Inclusief afsluitende slash.';
+$lang['enable_adding_urls_to_issues'] = 'Het toevoegen van externe verwijzingen naar issues aanzetten<br />Deze wijzen naar wikipagina\'s waarop de issues worden genoemd (Voorwaarde: configureer de API hieronder)';
+$lang['url_relationship']      = 'Relatie tussen de issue en de wikipagina<br />Wordt gebruikt om links te groeperen in de Jira interface.';
+$lang['jira_api_url']          = 'Jira REST API URL<br />Inclusief afsluitende slash.';
+$lang['jira_api_username']     = 'Jira gebruikersnaam voor toegang tot de Jira REST API';
+$lang['jira_api_password']     = 'Jira wachtwoord voor deze gebruiker';
