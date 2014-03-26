990bfe7bb3a1c3a131980c90d66de8bcda7370e4
Nicolas Friedli <nicolas@theologique.ch>
2014-03-26 11:40:35 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['jiraserver']            = 'URL du gestionnaire d\'erreurs Jira<br />Avec le slash final.';
+$lang['enable_adding_urls_to_issues'] = 'Activer la possiblité d\'ajouter des liens de rapports d\'erreur<br />Ces liens pointent vers les pages de wiki qui mentionnent ces erreurs (requiert une configuration de l\'API).';
+$lang['url_relationship']      = 'Relations entre la page des rapports d\'erreurs et la page de wiki<br />Utilisé pour regrouper les liens de l\'interface Jira.';
+$lang['jira_api_url']          = 'URL de l\'API REST Jira<br />Avec le slash final.';
+$lang['jira_api_username']     = 'Nom d\'utilisateur Jira utilisé pour accéder à l\'API REST Jira';
+$lang['jira_api_password']     = 'Mot de passe Jira pour cet utilisateur';
