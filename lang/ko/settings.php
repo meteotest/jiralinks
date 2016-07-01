23335f979b6ff7834b75c3b94b92925ca7d50dd4
Myeongjin <aranet100@gmail.com>
2016-07-01 08:35:30 +0200

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['jiraserver']            = 'Jira 이슈 브라우저 URL<br />뒤에 슬래시 포함';
+$lang['enable_adding_urls_to_issues'] = '원격 이슈 링크 추가를 활성화<br />이러한 이슈가 언급되어 있는 위키 문서를 가리킵니다 (구성된 API가 필요합니다.)';
+$lang['url_relationship']      = '이슈와 위키 문서 사이의 관계<br />Jira 인터페이스에서 링크를 함께 묶는데 사용됩니다.';
+$lang['jira_api_url']          = 'Jira REST API URL<br />뒤에 슬래시 포함';
+$lang['jira_api_username']     = 'Jira REST API에 접근하는 데 사용할 Jira 사용자 이름';
+$lang['jira_api_password']     = '이 사용자의 Jira 비밀번호';
