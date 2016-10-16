238c6a7130743f3b7422414aa4a47a43adbc724f
Arne Hanssen <arne.hanssen@getmail.no>
2016-10-16 11:35:23 +0200

    translation update

---
+++ lang/no/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Arne Hanssen <arne.hanssen@getmail.no>
+ */
+$lang['jiraserver']            = 'URL til Jira sin saksleser<br/>Inkl. skråstrek til slutt.';
+$lang['enable_adding_urls_to_issues'] = 'Slå på lenking tilbake til wiki-sider fra Jira. <br />Lenker tilbake til wikisiden der Jira-saken er nevnt. (Krever konfigurasjon i API)';
+$lang['url_relationship']      = 'Relasjon mellom en Jira-sak og en wikiside<br />Blir brukt for å gruppere linker i Jira-grensesnittet.';
+$lang['jira_api_url']          = 'Jira REST API URL<br />Inkl. skråstrek til slutt.';
+$lang['jira_api_username']     = 'Jira-brukernavn for tilgang til Jira sitt REST API';
+$lang['jira_api_password']     = 'Jira-passordet for denne bruker';
