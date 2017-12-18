b2509fa1c2fb992615799116f86c832ac7ae7881
Tor Härnqvist <tor@harnqvist.se>
2017-12-18 23:00:04 +0100

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['jira_api_url']          = 'Jira REST API URL<br />Inkl efterföljande snedstreck.';
+$lang['jira_api_username']     = 'Jira-användarnamn för åtkomst till Jira REST API';
+$lang['jira_api_password']     = 'Jira-lösenord för denna användare';
