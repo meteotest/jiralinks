7529c97c4520baf433f9a21b08cc41313a5c5259
Bartek S <sadupl@gmail.com>
2019-10-23 06:55:19 +0200

    translation update

---
+++ lang/pl/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Bartek S <sadupl@gmail.com>
+ */
+$lang['jira_api_username']     = 'Nazwa użytkownika Jira używana do dostępu do interfejsu Jira REST API';
+$lang['jira_api_password']     = 'Hasło Jira dla tego użytkownika';
